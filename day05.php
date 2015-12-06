26dba9f65b00736b5345dac8095267f75e997644
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-06 14:26:40 +0200

    Day 5 with test data

---
+++ day05.php
@@ -0,0 +1,41 @@
+<?php
+
+$strings = [
+  'ugknbfddgicrmopn',
+  'aaa',
+  'jchzalrnumimnmhp',
+  'haegwjzuvuyypxyu',
+  'dvszwmarrgswjxmb'
+];
+
+$naughty = 0;
+$nice = 0;
+foreach ($strings as $string) {
+  //It does not contain the strings ab, cd, pq, or xy,
+  //even if they are part of one of the other requirements.
+  if (preg_match('/ab|cd|pq|xy/', $string)) {
+    $naughty++;
+    continue;
+  }
+
+  //It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+  $vowels = preg_replace('/[^aeiou]/','', $string);
+  if (strlen($vowels) <3) {
+    $naughty++;
+    continue;
+  }
+
+  //It contains at least one letter that appears twice in a row,
+  //like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+  $chars = str_split($string);
+  for ($i = 0; $i < count($chars) - 1; $i++) {
+    if ($chars[$i] === $chars[$i+1]) {
+      $nice++;
+      continue(2);
+    }
+  }
+  $naughty++;
+}
+
+print("Naughty: ${naughty}\n");
+print("Nice: ${nice}\n");
