5d75fbed129978d0cb35df28bdd65dc4b385b517
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-06 11:25:34 +0200

    Count with robo santa

--- day03.php
+++ day03.php
@@ -1,16 +1,17 @@
 <?php
 $moves = str_split(file_get_contents('input/day03.txt'));
-$world = [0=>[0=>1]];
+$moves[] = 'end';
 $index = 0;
 $santa = [
-  0 => ['x' => 0, 'y' => 0]
+  0 => ['x' => 0, 'y' => 0],
+  1 => ['x' => 0, 'y' => 0]
 ];
 $count = count($santa);
+$world = [0=>[0=>$count]];
 
 //move santa
 foreach ($moves as $move) {
   $s = &$santa[$index++ % $count];
-  $world[$s['x']][$s['y']]++;
   switch ($move) {
     case '>':
       $s['x']++;
@@ -25,6 +26,7 @@ foreach ($moves as $move) {
       $s['y']--;
       break;
   }
+  $world[$s['x']][$s['y']]++;
 }
 
 //count houses
