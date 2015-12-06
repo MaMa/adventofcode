621631ffa2c94730630ad468713a32729b6f9007
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-06 10:45:28 +0200

    rename day2

--- day02.php
+++ day02.php
@@ -3,6 +3,7 @@ $input = file_get_contents('day02-input.txt');
 
 $rows = explode("\n", $input);
 $paper = 0;
+$ribbon = 0;
 foreach($rows as $row) {
   if (!$row) continue;
   $sides = explode('x', trim($row));
@@ -10,5 +11,14 @@ foreach($rows as $row) {
   $xz = ($sides[0] * $sides[2]);
   $yz = ($sides[1] * $sides[2]);
   $paper += 2*$xy + 2*$xz + 2*$yz + min([$xy, $xz, $yz]);
+
+  $xx = $sides[0] * 2;
+  $yy = $sides[1] * 2;
+  $zz = $sides[2] * 2;
+  $ribs = $xx + $yy + $zz - max([$xx, $yy, $zz]);
+  $bow = ($sides[0] * $sides[1] * $sides[2]);
+  $ribbon += $ribs + $bow;
 }
-print("Answer: ${paper}\n");
+
+print("Paper: ${paper}\n");
+print("Ribbon: ${ribbon}\n");
