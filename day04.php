67ac86033969da63096c1458838ae851c48e8321
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-06 14:03:22 +0200

    Day 4 part 2

--- day04.php
+++ day04.php
@@ -3,6 +3,6 @@ $key = 'iwrupvqb';
 $count = 0;
 do {
   $coin = md5($key . ++$count);
-} while (substr($coin, 0,5) !== '00000');
+} while (substr($coin, 0, 6) !== '000000');
 
 print("Coin ${coin} with count ${count}\n");
