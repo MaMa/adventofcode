e475b5582c9588ffa95486aa558e4ee753dc2b0c
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-06 16:20:14 +0200

    Day 5 part 2

--- day05.php
+++ day05.php
@@ -30,6 +30,52 @@ foreach ($strings as $string) {
   }
   $naughty++;
 }
+print("Part 1\n");
+print("Naughty: ${naughty}\n");
+print("Nice: ${nice}\n");
 
+// Part 2
+$naughty = 0;
+$nice = 0;
+foreach ($strings as $string) {
+  $len = strlen($string);
+  $chars = str_split($string);
+  //It contains a pair of any two letters that appears at least twice
+  //in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa),
+  //but not like aaa (aa, but it overlaps).
+  $match = false;
+  for ($i = 0; $i < $len - 3; $i++) {
+    $first = substr($string, $i, 2);
+    for ($j = $i+2; $j<$len-1; $j++) {
+      $second = substr($string, $j, 2);
+      if ($first === $second) {
+        $match = true;
+        break;
+      }
+    }
+  }
+  if (!$match) {
+    $naughty++;
+    continue;
+  }
+
+  //It contains at least one letter which repeats with exactly one letter
+  //between them, like xyx, abcdefeghi (efe), or even aaa.
+  $match = false;
+  for ($i = 0; $i < $len-2; $i++) {
+    $first = substr($string, $i, 1);
+    $second = substr($string, $i+2, 1);
+    if ($first === $second) {
+      $match = true;
+      break;
+    }
+  }
+  if (!$match) {
+    $naughty++;
+  } else {
+    $nice++;
+  }
+}
+print("\nPart 2\n");
 print("Naughty: ${naughty}\n");
 print("Nice: ${nice}\n");
