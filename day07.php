168b829b3d403111e05e941b6be791db90fac38c
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-07 20:10:28 +0200

    Solve Day07 part 1

--- day07.php
+++ day07.php
@@ -1,15 +1,6 @@
 <?php
 
-$input = [
-  '123 -> x',
-  '456 -> y',
-  'x AND y -> d',
-  'x OR y -> e',
-  'x LSHIFT 2 -> f',
-  'y RSHIFT 2 -> g',
-  'NOT x -> h',
-  'NOT y -> i'
-];
+$input = file('input/day07.txt');
 
 $circuit = [];
 
@@ -18,45 +9,48 @@ function num_or_val($in) {
   if (is_numeric($in)) {
     return $in & 0xFFFF;
   } else {
-    if (empty($circuit[$in])) {
-      var_dump($circuit[$match[1]]);
-      die('Not Set: '. $in);
+    if (is_null($circuit[$in])) {
+      throw new Exception('Not set '. $in);
     }
     return $circuit[$in];
   }
 }
 
-foreach($input as $in) {
+while($in = array_shift($input)) {
   if (!preg_match('/([\w ]+) -> (\w+)/', $in, $match)) continue;
   $code = $match[1];
   $to = $match[2];
-
-  if (is_numeric($code)) {
-    $circuit[$to] = intval($code) & 0xFFFF;
-  }
-  elseif (preg_match('/NOT (\w+)/', $code, $match)) {
-    $val = num_or_val($match[1]);
-    $circuit[$to] = ~intval($val) & 0xFFFF;
-  } elseif(preg_match('/(\w+) (AND|OR|LSHIFT|RSHIFT) (\w+)/', $code, $match)) {
-    $from = num_or_val($match[1]);
-    switch ($match[2]) {
-      case 'AND':
-        $circuit[$to] = $from & num_or_val($match[3]);
-        break;
-      case 'OR':
-        $circuit[$to] = $from | num_or_val($match[3]);
-        break;
-      case 'LSHIFT':
-        $circuit[$to] = $from << num_or_val($match[3]);
-        break;
-      case 'RSHIFT':
-        $circuit[$to] = $from >> num_or_val($match[3]);
-        break;
+  try {
+    if (preg_match('/^(\w+)$/', $code, $match)) {
+      $circuit[$to] = num_or_val($match[1]);
     }
-  } else {
-    die ('Invalid code '. $code);
+    elseif (preg_match('/NOT (\w+)/', $code, $match)) {
+      $circuit[$to] = ~num_or_val($match[1]);
+    } elseif(preg_match('/(\w+) (AND|OR|LSHIFT|RSHIFT) (\w+)/', $code, $match)) {
+      $from = num_or_val($match[1]);
+      switch ($match[2]) {
+        case 'AND':
+          $circuit[$to] = $from & num_or_val($match[3]);
+          break;
+        case 'OR':
+          $circuit[$to] = $from | num_or_val($match[3]);
+          break;
+        case 'LSHIFT':
+          $circuit[$to] = $from << num_or_val($match[3]);
+          break;
+        case 'RSHIFT':
+          $circuit[$to] = $from >> num_or_val($match[3]);
+          break;
+      }
+    } else {
+      die ('Invalid input: '. $in);
+    }
+  } catch (Exception $e) {
+    $input[] = $in; //loop to end of input
   }
 }
 
 ksort($circuit);
-var_dump($circuit);
+//var_dump($circuit);
+
+print("\nWire a: ". $circuit['a'] ."\n");
