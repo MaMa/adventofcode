0e3c94eb3e1d3013908d4a46763afda8120b5ec1
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-08 20:27:40 +0200

    Day 08 part 2

--- day08.php
+++ day08.php
@@ -4,8 +4,10 @@ $input = file('input/day08.txt');
 
 $mem = 0;
 $chars = 0;
+$escaped = 0;
 foreach($input as $in) {
   $string = trim($in);
+  if (empty($string)) continue;
   $mem += strlen($string);
   $clean = preg_replace('/\\\\x[0-9a-f]{2}/','X',
     str_replace('\\"', '"',
@@ -14,10 +16,14 @@ foreach($input as $in) {
       )
     )
   );
+
+  $enc = '"'. addslashes($string) .'"';
+  $escaped += strlen($enc);
   $chars += strlen($clean);
 }
 
 print("Mem ${mem}\n");
 print("Chars ${chars}\n");
-$diff = $mem - $chars;
-print("Diff ${diff}\n");
+print("Escaped ${escaped}\n");
+print("Mem - Chars: " . ($mem - $chars) ."\n");
+print("Escaped - Mem: " . ($escaped - $mem) ."\n");
