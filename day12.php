cd893ad2092345c5963cafcfdcd7eb8d0b76be85
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-12 20:10:06 +0200

    Day12 part 2

--- day12.php
+++ day12.php
@@ -7,9 +7,11 @@ function getSum($obj) {
   if (is_numeric($obj)) {
     $sum += $obj;
   } elseif (is_object($obj)) {
-    foreach ($obj as $key => $value) {
-      if (is_numeric($key)) $sum += $key;
-      $sum += getSum($value);
+    if (!hasRed($obj)) {
+      foreach ($obj as $key => $value) {
+        if (is_numeric($key)) $sum += $key;
+        $sum += getSum($value);
+      }
     }
   } elseif (is_array($obj)) {
     foreach ($obj as $value) {
@@ -19,4 +21,24 @@ function getSum($obj) {
   return $sum;
 }
 
+function hasRed($obj) {
+  if (is_string($obj)) return ($obj === 'red');
+  elseif (is_object($obj)) {
+    foreach ($obj as $val) {
+      if (is_string($val) && $val === 'red') return true;
+    }
+  }
+  return false;
+}
+
+function test($json) {
+  print($json .' -> '.getSum(json_decode($json)) ."\n");
+}
+
+test('[1,2,3]');
+test('[1,{"c":"red","b":2},3]');
+test('{"d":"red","e":[1,2,3,4],"f":5}');
+test('[1,"red",5]');
+
+//without reds
 var_dump(getSum($doc));
