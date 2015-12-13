f05d316d93ba6126280120a62f6d9920a7d2a25f
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-13 18:03:37 +0200

    Day13 part 1

--- day13.php
+++ day13.php
@@ -1,18 +1,7 @@
 <?php
 ini_set('memory_limit','256M');
 
-$in = ['Alice would gain 54 happiness units by sitting next to Bob.',
-'Alice would lose 79 happiness units by sitting next to Carol.',
-'Alice would lose 2 happiness units by sitting next to David.',
-'Bob would gain 83 happiness units by sitting next to Alice.',
-'Bob would lose 7 happiness units by sitting next to Carol.',
-'Bob would lose 63 happiness units by sitting next to David.',
-'Carol would lose 62 happiness units by sitting next to Alice.',
-'Carol would gain 60 happiness units by sitting next to Bob.',
-'Carol would gain 55 happiness units by sitting next to David.',
-'David would gain 46 happiness units by sitting next to Alice.',
-'David would lose 7 happiness units by sitting next to Bob.',
-'David would gain 41 happiness units by sitting next to Carol.'];
+$in = file('input/day13.txt');
 $regex = '/(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)./';
 
 $sit = [];
