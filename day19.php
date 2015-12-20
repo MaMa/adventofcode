7ee65de483a94cc372ff3534c8a1ae16b730ebe5
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-20 07:35:41 +0200

    Day19 - breadth first search did not work, need to try depth first

--- day19.php
+++ day19.php
@@ -1,5 +1,5 @@
 <?php
-
+ini_set('memory_limit','512M');
 const TEST = false;
 if (TEST) {
   $trans = [
@@ -20,18 +20,51 @@ if (TEST) {
   }
 }
 
-$results = [];
-$strLen = strlen($string);
-foreach ($trans as $key => $values) {
-  $keyLen = strlen($key);
-  for ($i=0; $i<=($strLen-$keyLen); $i++) {
-    if (substr($string, $i, $keyLen) === $key) {
-      $start = substr($string, 0, $i);
-      $end = substr($string, $i+$keyLen);
-      foreach($values as $replace) {
-        $results[] = $start . $replace . $end;
+function transform($trans, $string) {
+  $results = [];
+  $strLen = strlen($string);
+  foreach ($trans as $key => $values) {
+    $keyLen = strlen($key);
+    for ($i=0; $i<=($strLen-$keyLen); $i++) {
+      if (substr($string, $i, $keyLen) === $key) {
+        $start = substr($string, 0, $i);
+        $end = substr($string, $i+$keyLen);
+        foreach($values as $replace) {
+          $results[] = $start . $replace . $end;
+        }
       }
     }
   }
+  return array_unique($results);
 }
-print ("Uniques: ".count(array_unique($results)) ."\n");
+
+//Part 1 solution
+$part1 = transform($trans, $string);
+print ("Uniques: ".count($part1) ."\n");
+
+//reverse transformations
+$reverse = [];
+foreach($trans as $from => $values) {
+  foreach ($values as $val) {
+    if (isset($reverse[$val])) throw ('already exists');
+    $reverse[$val][] = $from;
+  }
+}
+
+$strings = [$string];
+$found = false;
+$count = 0;
+do {
+  $new = [];
+  foreach($strings as $string) {
+    $new = array_unique(array_merge($new, transform($reverse, $string)));
+  }
+  $strings = $new;
+  $count++;
+  printf("%d\t%d\n", $count, count($strings));
+  foreach($strings as $str) {
+    if ($str === 'e') $found = true;
+  }
+} while (!$found && $count < 10000);
+
+print ("Count ${count}\n");
