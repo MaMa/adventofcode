29f7e2630ae0199e95914a612e604873f6f33865
Markku Marjoneva <markku.marjoneva@gmail.com>
2015-12-25 08:31:37 +0200

    Day24 part 1

--- day24.php
+++ day24.php
@@ -1,89 +1,49 @@
 <?php
-
-//*
+ini_set('memory_limit', '256M');
+/*
 $in = [1,2,3,4,5,7,8,9,10,11];
 /*/
 $in = array_map('intval', file('input/day24.txt'));
 //*/
 
 $sum = array_sum($in);
-$third = $sum / 3;
-$splits = [];
+if ($sum % 3) die("Sum $sum is not divisible by 3\n");
+$target = $sum / 3;
 
-$result = [];
-pick($in, $sum / 3);
-$firsts = $result;
 
-foreach ($firsts as $first) {
-  sort($first);
-  $result = [];
-  $avs = array_filter($in, function($n) use ($first) {
-    return !in_array($n, $first);
-  });
-  pick($avs, $sum / 3);
+print ("Third of ${sum} = ${target}\n");
 
-  foreach($result as $second) {
-    sort($second);
-    $third = array_filter($in, function($n) use ($first, $second) {
-      return !in_array($n, $first) && !in_array($n, $second);
-    });
-    sort($third);
-    $splits[] = [$first, $second, $third];
-  }
-}
-
-$sers = [];
-foreach ($splits as $row) {
-  $sers[] = serialize($row);
-}
-
-$combos = [];
-foreach(array_unique($sers) as $ser) {
-  $combos[] = unserialize($ser);
-}
-
-usort($combos, function($row1, $row2) {
-  $c1 = count($row1[0]);
-  $c2 = count($row2[0]);
-  if ($c1 == $c2) {
-    $q1 = countQE($row1[0]);
-    $q2 = countQE($row1[0]);
-    if ($q1 == $q2) return 0;
-    else return ($q1 < $q2) ? -1 : 1;
-  }
-  else return ($c1 < $c2) ? -1 : 1;
-});
-
-function countQE($parts) {
-  return array_product($parts);
-}
-
-foreach ($combos as $row) {
-  foreach ($row as $c) {
-    print(implode(' ', $c) . ' | ');
-  }
-  print ("\n");
-}
-
-$QE = countQE($combos[0][0]);
-print("QE for first combo ". $QE . "\n");
-
-function pick($available, $target, $picked=[]) {
-  $diff = $target - array_sum($picked);
-  if ($diff === 0) {
-    global $result;
-    sort($picked);
-    $result[] = $picked;
-  } else foreach ($available as $num) {
+$results = [];
+$minPkgs = 999;
+function iter($packs, $res=[]) {
+  global $target, $results, $minPkgs;
+  $diff = $target - array_sum($res);
+  if ($diff == 0) {
+    $pkgs = count($res);
+    if ($pkgs < $minPkgs) {
+      $minPkgs = $pkgs;
+      $results = [$res];
+    } elseif ($pkgs == $minPkgs) {
+      $results[] = $res;
+    }
+  } else for ($i=0;$i<count($packs);$i++) {
+    $num = $packs[$i];
     if ($num <= $diff) {
-      $avaDiff = array_filter(
-        $available,
-        function($n) use ($num) {
-          return $num !== $n;
-        });
-      $pickNum = $picked;
-      $pickNum[] = $num;
-      pick($avaDiff, $target, $pickNum);
+      $newRes = $res;
+      $newRes[] = $num;
+      iter(array_slice($packs, $i+1), $newRes);
     }
   }
 }
+
+iter($in);
+var_dump($results);
+
+$minQE = NULL;
+foreach($results as $pkgs) {
+  $qe = array_product($pkgs);
+  if (!$minQE) $minQE = $qe;
+  elseif ($qe < $minQe) $minQe = $qe;
+}
+
+print ("Optimal QE: ${minQE}\n");
